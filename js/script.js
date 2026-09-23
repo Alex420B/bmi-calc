7f060a212547010f51f587e5d3a320c056079001
Alex420B <alexander04b@hotmail.com>
2026-09-23 22:27:29 +0200

    fandt ud af det

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict"
 // Husk fra dag 1: skriv "use strict" herunder
 
 
@@ -8,10 +9,11 @@
 // Vigtigt: i JavaScript skrives decimaltal med PUNKTUM, ikke komma. Skriv altså 1.75, ikke 1,75.
 // Skriver du 1,75, opfatter JavaScript det som to separate værdier (1 og 75), og koden vil ikke
 // give det resultat, du forventer.
-function beregnBMI() {
+function beregnBMI(weight, height) {
+    
 
     // Eksempel: BMI beregnes med formlen vægt divideret med (højde multipliceret med højde)
-    let bmi = weight / (height * height);
+    const bmi= weight / (height*height);
 
     // Skriv resten af if/else if-kæden selv herunder. Den første gren er startet for dig.
     //
@@ -19,24 +21,49 @@ function beregnBMI() {
     // intervallet), mens < betyder "mindre end" (tallet selv er IKKE inkluderet). Fx hører en
     // bmi på præcis 18.5 til normalvægt, ikke undervægt - fordi normalvægt starter ved >= 18.5
     // (inklusiv), mens undervægt kun gælder for værdier under 18.5 (eksklusiv).
-    if (bmi < 18.5) {
+    if (bmi <= 18.5) (weight) && (height)
+        
         // Skriv selv: log `Din BMI er ${bmi}. Det betyder undervægt.`
-
-    }
+    
+    
+   
     // Skriv selv: tilføj en else if-gren, der tjekker om bmi er 18.5 eller derover, OG mindre end 25.
     // Log i så fald: `Din BMI er ${bmi}. Det betyder normalvægt.`
+function beregnBMI(weight, height) {
+    const bmi = weight / (height * height); 
+}
+
+    if (bmi < 18.5) {
+        console.log(`Din BMI er ${bmi}. Det betyder undervægt.`);
+    } 
+    
+    else if (bmi >= 18.5 && bmi < 25) {
+        console.log(`Din BMI er ${bmi}. Det betyder normalvægt.`);
+    } 
 
 
     // Skriv selv: tilføj en else if-gren, der tjekker om bmi er 25 eller derover, OG mindre end 30.
     // Log i så fald: `Din BMI er ${bmi}. Det betyder overvægt.`
-
+else if (bmi >= 25 && bmi < 30) {
+        console.log(`Din BMI er ${bmi}. Det betyder overvægt.`);
+    } 
 
     // Skriv selv: tilføj en sidste else-gren.
-    // Log: `Din BMI er ${bmi}. Det betyder svær overvægt.`
+    // Log: `Din BMI er ${bmi}. Det betyder svær overvægt.
+    // `
+else { (bmi > 30)
+        console.log(`Din BMI er ${bmi}. Det betyder svær overvægt.`);
+    }
+
 
 }
 
+
 beregnBMI(70, 1.75);
+beregnBMI(55, 1.70);
+beregnBMI(90, 1.75);
+beregnBMI(100, 1.70);
+
 // Skriv selv: kald funktionen 2-3 gange mere med andre kombinationer af weight og height,
 // så du selv kan se de øvrige kategorier blive testet i konsollen.
 //
